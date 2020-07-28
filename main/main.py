c5dfc9a2b013f5bfd860a2085cb599a3d77266d5
Alessio <alessio.mazzola.95@gmail.com>
2020-07-28 11:32:09 +0200

    new problems added

--- main/main.py
+++ main/main.py
@@ -14,31 +14,23 @@ import time
 #ListOfModules --> List[]: Rappresenta la lista dei moduli richiesti
 #ListOfDemands --> List[]: Rappresenta la lista delle domande associate ai moduli
 #n --> int: Rappresenta la lunghezza della lista 
-L = 110
-listOfModules = [70,40,55,25,35]
-listOdDemands = [205,2321,143,1089,117]
+#L = 110
+#listOfModules = [70,40,55,25,35]
+#listOfDemands = [205,2321,143,1089,117]
 #L = 20
 #listOfModules = [9,8,7,6]
-#listOdDemands = [511,301,263,383]
-#L = 10000
-#listOfModules = [4990,4919,4640,4300,4283,4225,4108,4097,4071,3971,3898,3875,3853,3833,3747,3701,3660,3388,3019,2927,2832,2823,2696,2408,1585,1546,1222,1194,1096,1049,958,876,865,835,676,636,613,583,527,515,506,484,478,425,294,186,64]
-#listOdDemands = [2,3,2,3,2,3,4,2,4,2,1,5,1,1,2,2,2,1,1,2,1,2,1,2,1,2,3,1,3,1,1,1,1,2,2,1,3,3,2,3,1,5,1,1,1,5,1] ##run1
-#listOdDemands = [200,300,200,300,200,300,400,200,400,200,100,500,100,100,200,200,200,100,100,200,100,200,100,200,100,200,300,100,300,100,100,100,100,200,200,100,300,300,200,300,100,500,100,100,100,500,100] ##run2
-#L = 10000
-#listOfModules = [4964,4950,4826,4816,4808,4682,4663,4631,4451,4387,4233,4127,3992,3968,3858,3698,3672,3565,3387,3336,3309,3118,3078,2988,2870,2862,2847,2735,2716,2652,2631,2553,2496,2406,2125,2017,1939,1848,1832,1672,1624,1593,1436,1225,1212,1060,943,763,733,583,476,462,278,246,80,74,40]
-#listOdDemands = [3,1,1,1,1,1,4,1,1,2,1,1,2,1,1,2,4,1,3,3,1,3,4,1,1,2,2,2,3,1,3,2,3,1,2,1,3,4,1,2,1,1,4,2,3,4,2,1,4,1,1,2,3,1,3,2,2]
-#L = 10000
-#listOfModules = [4990,4919,4640,4300,4283,4225,4108,4097,4071,3971,3898,3875,3853,3833,3747,3701,3660,3388,3019,2927,2832,2823,2696,2408,1585,1546,1222,1194,1096,1049,958,876,865,835,676,636,613,583,527,515,506,484,478,425,294,186,64]
-#listOdDemands = [2,3,2,3,2,3,4,2,4,2,1,5,1,1,2,2,2,1,1,2,1,2,1,2,1,2,3,1,3,1,1,1,1,2,2,1,3,3,2,3,1,5,1,1,1,5,1]
-
+#listOfDemands = [511,301,263,383]
+L = 10000
+listOfModules = [2487,2439,2431,2419,2400,2333,2303,2232,2192,2176,2007,1817,1704,1612,1557,1457,1454,1304,1255,1246,1219,1148,1142,1066,1010,987,969,948,923,685,679,663,598,589,548,537,513,385,303,248,106,2]
+listOfDemands = [2,1,2,3,2,1,2,2,1,2,2,3,1,3,3,2,3,3,2,1,3,2,1,2,5,2,3,5,3,2,2,3,1,1,4,2,5,4,2,2,4,19] ##run1
 ##MIE
 #L = 320
 #listOfModules = [20,60,80,100,160]
-#listOdDemands = [12,7,9,45,52]
+#listOfDemands = [12,7,9,45,52]
 
 #L = 1500
 #listOfModules = [1, 2, 4, 5, 8, 10, 20, 25, 40, 50, 100, 125, 200, 250, 500, 1000]
-#listOdDemands = [158,240,7,56,78,65,30,15,550,664,768,255,11,1,1500,12]
+#listOfDemands = [158,240,7,56,78,65,30,15,550,664,768,255,11,1,1500,12]
 
 
 n = len(listOfModules)
@@ -103,6 +95,9 @@ def calculate_solution_substitute_method(L, listOfModules, listOdDemands, n):
 
 def calculate_solution_add_method(L, listOfModules, listOdDemands, n):
 
+    print(f"Lunghezza lista dei moduli: {len(listOfModules)}")
+    print(f"Lunghezza lista della domanda: {len(listOdDemands)}")
+
     counter = 0
     ##Passo 0: inizializzazione
     B = cs.determineInitialPattern(L,listOfModules)
@@ -148,4 +143,4 @@ def calculate_solution_add_method(L, listOfModules, listOdDemands, n):
 
 
 #calculate_solution_substitute_method(L, listOfModules, listOdDemands, n)
-calculate_solution_add_method(L, listOfModules, listOdDemands, n)
+calculate_solution_add_method(L, listOfModules, listOfDemands, n)
